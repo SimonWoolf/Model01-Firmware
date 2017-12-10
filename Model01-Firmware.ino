d5b92820b1bf7e30852f4f9c0ee36d641451ed7b
Simon Woolf <simon@ably.io>
2017-12-10 19:50:55 +0100

    Add parens as fn+m,

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -161,15 +161,15 @@ const Key keymaps[][ROWS][COLS] PROGMEM = {
   [FUNCTION] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_LEDEffectNext,
    Key_Tab,  Key_Home,         Key_UpArrow, Key_End,    M(MACRO_ALTSYSRQ),     ___, ___,
-   Key_Home, Key_LeftArrow,    Key_DownArrow, Key_RightArrow, Key_mouseBtnL, Key_mouseWarpNW,
-   Key_End,  Key_PrintScreen,  Key_Insert,  Key_Pause,        Key_mouseBtnM, Key_NonUsBackslashAndPipe,  Key_mouseWarpSE,
+   Key_Home, Key_LeftArrow,    Key_DownArrow, Key_RightArrow, ___, ___,
+   Key_End,  Key_PrintScreen,  Key_Insert,  Key_Pause,        Consumer_VolumeDecrement, Consumer_VolumeIncrement,  ___,
    ___, Key_Escape, ___, Key_LeftAlt,
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
    Consumer_PlaySlashPause,    Key_DownArrow,          Key_RightArrow,           ___,                      ___,              ___,            Key_F12,
                                Key_LeftArrow,          Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket,              ___,
-   Key_PcApplication,          Key_UpArrow,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   Key_PcApplication,          Key_UpArrow,            Key_KeypadLeftParen, Key_KeypadRightParen, ___,             Key_Backslash,    Key_Pipe,
    Key_RightAlt, ___, Key_Backspace, ___,
    ___)
 
